ecf1e19c36220dcbfd1176fc06b34516d4811379
Kor Dwarshuis <proximus@fastmail.com>
2025-03-12 09:56:00 +0100

    Add more comparisons

--- src/compare.mjs
+++ src/compare.mjs
@@ -9,6 +9,10 @@ export async function compareFiles(config) {
     const flatA = config.objectA.flat();
     const flatB = config.objectB.flat();
 
+    // Entries in B that are also in A
+    const inBoth = flatB.filter(entry => flatA.includes(entry));
+    const stringResult = `Entries in both “${config.objectAname.name}” and “${config.objectBname.name}”: ` + JSON.stringify(inBoth, null, 2);
+
     // Find entries in objectA that are not in objectB
     flatA.forEach(entry => {
         if (!flatB.includes(entry)) {
@@ -23,16 +27,26 @@ export async function compareFiles(config) {
         }
     });
 
-    // Entries in B that are also in A
-    const inBoth = flatB.filter(entry => flatA.includes(entry));
-    const stringResult = `Entries in both “${config.objectAname.name}” and “${config.objectBname.name }”: ` + JSON.stringify(inBoth, null, 2);    
+    
+    const jsonResultInBoth = JSON.stringify(inBoth, null, 2);
+    const jsonResultNotInA = JSON.stringify(notInA, null, 2);
+    const jsonResultNotInB = JSON.stringify(notInB, null, 2);
 
     console.log('\n\n');
     console.log(stringResult);
 
     // Write stringResult to a file
     await fs
-        .writeFile(path.join('.', 'result.txt'), stringResult)
+        .writeFile(path.join('.', 'result-in-both.txt'), stringResult)
+        .catch(console.error);
+    await fs
+        .writeFile(path.join('.', 'result-in-both.json'), jsonResultInBoth)
+        .catch(console.error);
+    await fs
+        .writeFile(path.join('.', 'result-not-in-a.json'), jsonResultNotInA)
+        .catch(console.error);
+    await fs
+        .writeFile(path.join('.', 'result-not-in-b.json'), jsonResultNotInB)
         .catch(console.error);
     
     return { notInB, notInA, inBoth };
