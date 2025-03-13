0294a0877f08cf3a6a8116316f0a5d1c2489c74d
Kor Dwarshuis <proximus@fastmail.com>
2025-03-13 15:57:45 +0100

    Skipping user input if config.js exists + refactor code

--- userInput.mjs
+++ userInput.mjs
@@ -1,67 +1,40 @@
-import { promises as fs } from 'fs';
+import fs from 'fs';
+import path from 'path';
 import readline from 'readline';
 
-let config = {};
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-function askForToken() {
-    return new Promise((resolve) => {
-        rl.question('Please enter your GitHub API token: ', (token) => {
-            resolve(token);
-        });
-    });
-}
-
-function askForRepoA() {
-    return new Promise((resolve) => {
-        rl.question('Please enter URL to repo A terms dir: ', (urlRepoA) => {
-            resolve(urlRepoA);
-        });
-    });
-}
-
-function askForRepoB() {
-    return new Promise((resolve) => {
-        rl.question('Please enter URL to repo B terms dir: ', (urlRepoB) => {
-            resolve(urlRepoB);
-        });
-    });
-}
-
-async function getGithubToken() {
-    let token = process.env.GITHUB_API_TOKEN;
-    if (!token) {
-        token = await askForToken();
-    }
-    return token;
-}
-
-async function getRepoA() {
-    return await askForRepoA();
-}
-
-async function getRepoB() {
-    return await askForRepoB();
-}
-
 export async function getUserInput() {
-    config.token = await getGithubToken();
-    config.outputDir = './compare-spec-up-t-repos-output';
-    config.repoA = {};
-    config.repoB = {};
-    config.repoA.name = "repo-A";
-    config.repoB.name = "repo-B";
-    config.repoA.url = await getRepoA();
-    config.repoB.url = await getRepoB();
+    const configPath = path.join(process.cwd(), 'config.js');
 
-    // Close the readline interface after all input has been collected
+    if (fs.existsSync(configPath)) {
+        console.log('config.js already exists. Skipping user input.');
+        return;
+    }
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    const question = (query) => new Promise((resolve) => rl.question(query, resolve));
+
+    const token = await question('Enter your token: ');
+    const outputDir = await question('Enter the output directory: ');
+    const repoAname = await question('Enter the name of repo A: ');
+    const repoAurl = await question('Enter the URL of repo A: ');
+    const repoBname = await question('Enter the name of repo B: ');
+    const repoBurl = await question('Enter the URL of repo B: ');
+
     rl.close();
 
-    // Convert the config object to a JavaScript variable assignment
-    const jsContent = `const config = ${JSON.stringify(config, null, 2).replace(/"([^"]+)":/g, '$1:')};\nmodule.exports = config;`;
-    await fs.writeFile('config.js', jsContent, 'utf8');
+    const configContent = `
+        module.exports = {
+            token: '${token}',
+            outputDir: '${outputDir}',
+            repoA: { name: '${repoAname}', url: '${repoAurl}' },
+            repoB: { name: '${repoBname}', url: '${repoBurl}' }
+        };
+    `;
+
+    fs.writeFileSync(configPath, configContent.trim());
+    console.log('config.js has been created.');
 }
